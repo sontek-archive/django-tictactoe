06d6de451898e5be72b1367a6969adbceb316d04
John Anderson <sontek@gmail.com>
2011-03-13 19:46:37 -0400

    fixed a swap user issue with the javascript

--- tictactoe/static/game.js
+++ tictactoe/static/game.js
@@ -4,15 +4,11 @@ function MakeMove(sender, move) {
     if (player == current_player) {
         if ($(sender).text().trim() == "" && $("#messages").text().trim() == "") {
             $(sender).html(player);
-
-            if (current_player == player) {
-                current_player = computer;
-            } else {
-                current_player = player;
-            }
+            SwapUser();
 
             $.post(create_move_url, {'move': move},
                 function(data) {
+                    SwapUser();
                     if(data[0] != '') {
                         $("#cell" + data[0]).html(computer);
                     }
@@ -34,4 +30,12 @@ function SetMessage(message) {
     $("#messages").html(message);
     $("#messages").show();
     $("#playagain").show();
+}
+
+function SwapUser() {
+    if (current_player == player) {
+        current_player = computer;
+    } else {
+        current_player = player;
+    }
 }
